1043f652ef6648af1bcf3621887fb68acd117376
uglykoyote <uglykoyote@yandex.ru>
2018-05-22 15:01:25 -0400

    Add new exercise

---
+++ Warmup1/parrot_trouble/parrot_trouble.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def parrot_trouble(talking, hour):
+    safe_hour = list(range(7,21,1))
+    if safe_hour.count(hour):
+        return False
+    else:
+        if talking:
+            return True
+        else:
+            return False
+
+
+# My little tests
+def main():
+    parrot_trouble(True, 6)
+    parrot_trouble(True, 7)
+    parrot_trouble(False, 6)
+    parrot_trouble(True, 20)
+
+
+if __name__ == '__main__':
+    main()
