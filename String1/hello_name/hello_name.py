49417f3a46fdfe43a4167bbd063dc427d775c0f1
Uglykoyote <uglykoyote@yandex.ru>
2018-05-23 14:42:42 +0400

    Update hello_name.py

---
+++ String1/hello_name/hello_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# My solution
+def hello_name(name):
+    return 'Hello ' + name + '!'
+
+
+# My little tests
+def main():
+    hello_name('Bob')
+    hello_name('Alice')
+    hello_name('X')
+
+
+if __name__ == '__main__':
+    main()
