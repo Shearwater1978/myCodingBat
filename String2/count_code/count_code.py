224f4270cd0b942f3ebaf606a0ad4c6ac62a2add
Uglykoyote <uglykoyote@yandex.ru>
2018-05-29 16:05:27 +0400

    Create count_code.py

---
+++ String2/count_code/count_code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def count_code(str):
+    split_on = count = 0
+    while len(str) >= split_on + 4:
+        if (str[split_on:split_on + 2] == 'co' and str[split_on + 3] == 'e'):
+            count += 1
+        split_on += 1
+    return count
+
+# My little tests
+def main():
+    count_code_new('codexxcode')
+#    count_code('aaacodebbb')
+#    count_code('codexxcode')
+#    count_code('cozexxcope')
+
+
+if __name__ == '__main__':
+    main()
+
