5e0853645a99a4ecce453eb1cbc5b4943a6e0bce
Uglykoyote <uglykoyote@yandex.ru>
2018-05-31 11:17:08 +0400

    Update date_fashion.py

--- Logic-1/date_fashion/date_fashion.py
+++ Logic-1/date_fashion/date_fashion.py
@@ -1,1 +1,23 @@
-#
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def date_fashion(you, date):
+    if you <= 2 or date <= 2:
+        return (0)
+    elif (you in list(range(8, 11)) or date in list(range(8, 11))):
+        return (2)
+    else:
+        return (1)
+
+
+# My little tests
+def main():
+    date_fashion(5, 10)
+    date_fashion(9, 2)
+    date_fashion(5, 5)
+
+
+if __name__ == '__main__':
+    main()
+
