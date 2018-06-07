27d5c46add5b73543ce8699aaff5b3bb7d7a0350
Uglykoyote <uglykoyote@yandex.ru>
2018-06-07 08:13:28 +0400

    Create string_times.py

---
+++ Warmup2/string_times/string_times.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def string_times(str, n):
+  return(str * n)
+
+
+# My little tests
+def main():
+    string_times('Hi', 2)
+    string_times('Hi', 3)
+    string_times('Hi', 1)
+
+
+if __name__ == '__main__':
+    main()
+
