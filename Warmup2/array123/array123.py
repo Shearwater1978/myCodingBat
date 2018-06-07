3393bab8cb422b06a1d8ccb255e529c0aecf8720
Uglykoyote <uglykoyote@yandex.ru>
2018-06-07 09:29:55 +0400

    Create array123.py

---
+++ Warmup2/array123/array123.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def array123(nums):
+    res = False
+    for count, num in enumerate(nums):
+        if num == 1 and count + 2 < len(nums):
+            if nums[count+1] == 2 and nums[count+2] == 3:
+                res = True
+            else:
+                res = False
+    return (res)
+
+# My little tests
+def main():
+    array123([1, 1, 2, 3, 1])
+    array123([1, 1, 2, 4, 1])
+    array123([1, 1, 2, 1, 2, 3])
+
+
+if __name__ == '__main__':
+    main()
+
